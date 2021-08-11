629a59e25bcfe8744e60f287f73d4382c5421ed5
pleshw <pleshw@outlook.com>
2021-08-11 16:30:18 -0300

    Update string-utils.js

--- string-utils.js
+++ string-utils.js
@@ -6,17 +6,3 @@ function primeiraLetraMaiuscula(str) {
     if (typeof s !== 'string') return '';
     return s.charAt(0).toUpperCase() + s.slice(1);
 }
-
-function rejeitarEspacos(htmlInputElement) {
-    inputElement.addEventListener("keyup", function () {
-        this.value = this.value.replace(/(\s)*/igm, "");
-    })
-
-    inputElement.addEventListener("keydown", function () {
-        this.value = this.value.replace(/(\s)*/igm, "");
-    });
-
-    inputElement.addEventListener("paste", function () {
-        this.value = this.value.replace(/(\s)*/igm, "");
-    });
-}
